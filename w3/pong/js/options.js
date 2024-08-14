e286fa79be7264d587742b09ed1e899a34b585db
DriscollDev <cdriscolldev@gmail.com>
2024-08-14 11:59:21 -0400

    partial

--- w3/pong/js/options.js
+++ w3/pong/js/options.js
@@ -9,6 +9,7 @@ optionButton.addEventListener(`click`, function(){
     document.querySelector(`.sides`).classList.toggle(`hidden`);
 });
 
+
 /*---------
     Program the two fill inputs to do the following:
     . Display the correct colors on the inputs and outputs and paddles    
@@ -19,6 +20,19 @@ optionButton.addEventListener(`click`, function(){
 
 -----------*/
 
+var fillInputs = document.querySelectorAll(`.fill`);
+for(let i=0; i<fillInputs.length; i++)
+{
+    fillInputs[i].value = player[i].fill;
+    fillInputs[i].nextSibling.style.backgroundColor = fillInputs[i].value;
+    player[i].pad.fill = fillInputs[i].value;
+    fillInputs[i].addEventListener(`input`, function(){
+        player[i].pad.fill = this.value;
+        fillInputs[i].nextSibling.style.backgroundColor = this.value;
+    });
+}
+
+
 /*---------
     Program the six key inputs to do the following:
     . Display the correct key names for each player   
@@ -27,3 +41,20 @@ optionButton.addEventListener(`click`, function(){
         .Change the player's key to the value of the input
         .Show the player's key in the output div 
 -----------*/
+
+
+
+var upInputs = document.querySelectorAll(`.u`);
+for(let i=0; i<upInputs.length; i++)
+{
+    upInputs[i].value = player[i].keys.u;
+    upInputs[i].nextSibling.innerText = player[i].keys.u;
+    upInputs[i].addEventListener(`keydown`, function(event){
+        player[i].keys.u = event.key;
+        upInputs[i].value = player[i].keys.u;
+        upInputs[i].nextSibling.innerText = player[i].keys.u;
+    });
+    upInputs[i].addEventListener(`focus`, function(){
+        currentState = `pause`;
+    });
+}
